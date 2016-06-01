27fa17233b4185fda3b99e80c53c390b34839718
Steve Hsu <saint@aliyun.com>
2016-06-01 15:53:38 +0800

    fix conflicts

---
+++ include/Aiks.h
@@ -0,0 +1,96 @@
+/*************************************
+ * Copyright 2016 Aiks Group
+ * ***********************************/
+
+#ifndef AIKS_AIKS_H
+#define AIKS_AIKS_H
+
+// define supported target platform macro which Aiks uses.
+#define AK_PLATFORM_UNKNOWN            0
+#define AK_PLATFORM_IOS                1
+#define AK_PLATFORM_ANDROID            2
+
+#define AK_PLATFORM_WIN32              3
+#define AK_PLATFORM_MARMALADE          4
+#define AK_PLATFORM_LINUX              5
+#define AK_PLATFORM_BADA               6
+#define AK_PLATFORM_BLACKBERRY         7
+#define AK_PLATFORM_MAC                8
+#define AK_PLATFORM_NACL               9
+#define AK_PLATFORM_EMSCRIPTEN        10
+#define AK_PLATFORM_TIZEN             11
+#define AK_PLATFORM_WINRT             12
+#define AK_PLATFORM_WP8               13
+
+#define AK_TARGET_PLATFORM  AK_PLATFORM_UNKNOWN
+
+// mac
+#if defined(AK_TARGET_OS_MAC) || defined(__APPLE__)
+#undef  AK_TARGET_PLATFORM
+#define AK_TARGET_PLATFORM         AK_PLATFORM_MAC
+#endif
+
+// iphone
+#if defined(AK_TARGET_OS_IOS)
+#undef  AK_TARGET_PLATFORM
+#define AK_TARGET_PLATFORM         AK_PLATFORM_IOS
+#endif
+
+// android
+#if defined(ANDROID)
+#undef  AK_TARGET_PLATFORM
+#define AK_TARGET_PLATFORM         AK_PLATFORM_ANDROID
+#endif
+
+// WinRT (Windows Store App)
+#if defined(WINRT) && defined(_WINRT)
+#undef  AK_TARGET_PLATFORM
+#define AK_TARGET_PLATFORM			AK_PLATFORM_WINRT
+#endif
+
+// WP8 (Windows Phone 8 App)
+#if defined(WP8) && defined(_WP8)
+#undef  AK_TARGET_PLATFORM
+#define AK_TARGET_PLATFORM			AK_PLATFORM_WP8
+#endif
+
+// win32
+#if defined(WIN32) && defined(_WINDOWS)
+#undef  AK_TARGET_PLATFORM
+#define AK_TARGET_PLATFORM         AK_PLATFORM_WIN32
+#endif
+
+// linux
+#if defined(LINUX) && !defined(__APPLE__)
+#undef  AK_TARGET_PLATFORM
+#define AK_TARGET_PLATFORM         AK_PLATFORM_LINUX
+#endif
+
+// tizen
+#if defined(TIZEN)
+#undef  AK_TARGET_PLATFORM
+#define AK_TARGET_PLATFORM     AK_PLATFORM_TIZEN
+#endif
+
+#if ! AK_TARGET_PLATFORM
+#error "Cannot recognize the target platform"
+#endif
+
+//Aiks Header
+
+//SH Version Header
+#include "draw/BasicDraw.h"
+#include "draw/Texture.h"
+#include "draw/Vertex3D.h"
+#include "light/FakeLight.h"
+#include "light/SimpleDiffuseLight.h"
+#include "math/Matrix44.h"
+#include "math/Transform.h"
+
+#if AK_TARGET_PLATFORM == AK_PLATFORM_MAC
+#include "device/osx/MacDevice.h"
+#include "device/osx/SHHardwareCanvas.h"
+#include "device/osx/SHSoftwareCanvas.h"
+#endif
+
+#endif
